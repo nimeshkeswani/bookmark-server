dc70a9ff44399270821c04266490cc926cd9d10d
Nimesh Keswani <nimeshkeswani@hotmail.com>
2017-12-09 10:54:49 -0800

    use PORT from environment

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -41,6 +41,7 @@
 #
 # After writing each step, restart the server and run test.py to test it.
 
+import os
 import http.server
 import requests
 from urllib.parse import unquote, parse_qs
@@ -152,6 +153,7 @@ class Shortener(http.server.BaseHTTPRequestHandler):
             #raise NotImplementedError("Step 5 isn't written yet!")
 
 if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 8000))
     server_address = ('', 8000)
     httpd = http.server.HTTPServer(server_address, Shortener)
     httpd.serve_forever()
